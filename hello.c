9320ef358796881f56be6277141100c72870348b
sanqingyiniao <862294604@qq.com>
2023-09-16 22:52:47 +0800

    0-intro

---
+++ hello.c
@@ -0,0 +1,15 @@
+//
+// Created by Lenovo on 2023/9/16.
+//
+
+//#include <stdio.h>
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+
+    // return statement (return to operating system)
+    // 0: exit code
+    return 0;
+}
